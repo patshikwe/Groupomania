69178e39ddcc62c2cb4427e4735ad1eae5284d2a
patshikwe <patshikwe@yahoo.fr>
2022-06-09 00:06:14 +0200

    Lancement de l'appli React

---
+++ groupomania-network/src/components/App.js
@@ -0,0 +1,17 @@
+import logo from '../assets/logo/icon-left-font-monochrome-black.svg';
+
+
+function App() {
+  return (
+    <div >
+      <header className="App-header">
+        <img src={logo} className="" alt="logo" />
+        <p>
+          Bienvenue dans votre espace d'échange entre collègues!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
