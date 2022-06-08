d8bdf19572d134d5720cc65903a1cb561d789508
patshikwe <patshikwe@yahoo.fr>
2022-06-09 00:08:55 +0200

    Modification de username en unique

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,11 +8,11 @@ const uniqueValidator = require('mongoose-unique-validator');
 
 /* la propriété unique de uniqueValidator empêchent 
    plusieurs enregistrements  d'un même compte utilisateur.
-*/ 
+*/
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  username: { type:String, required: true}
+  username: { type: String, required: true, unique: true }
 });
 
 userSchema.plugin(uniqueValidator);
