d51c8a4e4e5b01d1296b245ea410b0ee8896a861
patshikwe <patshikwe@yahoo.fr>
2022-06-13 12:08:24 +0200

    Mise en place du logo dans header page d'accueil

--- groupomania-network/src/pages/Home.js
+++ groupomania-network/src/pages/Home.js
@@ -1,13 +1,19 @@
 // Page d'accueil(Home page)
 
 import React from 'react'
+import logo from '../assets/logo/icon-left-font-monochrome-black.svg'
 
 const Home = () => {
     return (
         <div>
-            Ici Acueil Home
+            <header className="">
+                <img src={logo} className="logo" alt="logo" />
+            </header>
+            <div>
+                Ici Acueil Home
+            </div>
         </div>
     )
-}
+} 
 
 export default Home
--- groupomania-network/src/pages/Profil.js
+++ groupomania-network/src/pages/Profil.js
@@ -3,26 +3,18 @@ import React from 'react'
 import logo from '../assets/logo/icon-left-font-monochrome-black.svg';
 import Log from '../components/Log'
 import '../styles/pages/Profil.css'
-import { useContext } from 'react';
-import { UidContext } from '../components/AppContext';
 
 const Profil = () => {
-    const uid = useContext(UidContext)
+    
 
     return (
-        <div className=''>
-            {uid? (
-                <h1>UpdateProfil</h1>
-            ) : (
-                    <div className=''>
-                    <header className="">
-                        <img src={logo} className="logo" alt="logo" />
-                    </header>
-                    <div className=''>
-                        <Log login={false} signup={true}/>
-                    </div>
-                </div>
-            ) }   
+        <div className=''>   
+            <header className="">
+                <img src={logo} className="logo" alt="logo" />
+            </header>
+            <div className=''>
+                <Log login={false} signup={true}/>
+            </div>
         </div>
     )
 }
