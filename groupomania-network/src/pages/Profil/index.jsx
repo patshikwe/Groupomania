c375e8715add307d9ac77ebf27c9f5e851cf306b
patshikwe <patshikwe@yahoo.fr>
2022-07-14 20:28:12 +0200

    Correction

---
+++ groupomania-network/src/pages/Profil/index.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react'
+import logo from '../../assets/logo/icon-left-font-monochrome-black.svg'
+import Log from '../../components/Log'
+import '../../styles/pages/Profil.css'
+
+const index = () => {
+    
+
+    return (
+        <div className=''>   
+            <header className="">
+                <img src={logo} className="logo" alt="logo" />
+            </header>
+            <div className=''>
+                <Log login={false} signup={true}/>
+            </div>
+        </div>
+    )
+}
+
+export default index
