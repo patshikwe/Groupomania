0c2acae1480cd33b1be03941a6fcf516848e044c
patshikwe <patshikwe@yahoo.fr>
2022-08-08 00:16:07 +0200

    Création de route pour récupérer tous les users

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,13 +5,18 @@ const express = require('express')
 const router = express.Router()
 const userCtrl = require('../controllers/user')
 const validPassword = require('../middleware/valid-password')
+const userController = require('../controllers/user-controller')
 
 /* Routes
    userCtrl -> chemin vers controllers/user.js
    signup -> correspond à exports.signup
    login -> correspond à exports.login
+   validPassword -> chemin vers validation de mot de passe
 */
 router.post('/register', validPassword, userCtrl.signup)
 router.post('/login', userCtrl.login)
+
+// Option utilisateurs
+router.get('/', userController.getAllUsers)
 
 module.exports = router
