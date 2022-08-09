a838b6c81c3d122c9fd12a29982c8e83ee657572
patshikwe <patshikwe@yahoo.fr>
2022-08-09 02:43:01 +0200

    Post model

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -23,16 +23,14 @@ exports.createPost = (req, res, next) => {
 
 // Récupérer toutes les publications (posts) *****************
 exports.getAllPosts = (req, res, next) => {
-  post
-    .find()
+  Post.find()
     .then((posts) => res.status(200).json(posts))
     .catch((error) => res.status(400).json({ error }))
 }
 
 // Récupérer une publication (post) *****************
 exports.getOnePost = (req, res, next) => {
-  post
-    .findOne({ _id: req.params.id })
+  Post.findOne({ _id: req.params.id })
     .then((post) => res.status(200).json(post))
     .catch((error) => res.status(404).json({ error }))
 }
