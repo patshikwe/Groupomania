35ca84aafc590ce7f6062182e91b44c82d5cb40a
patshikwe <patshikwe@yahoo.fr>
2022-08-10 23:53:57 +0200

    Commentaires

--- groupomania-network/src/index.js
+++ groupomania-network/src/index.js
@@ -1,9 +1,12 @@
+// Fichier de connexion avec index.html
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import '../src/styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
 
+// div id="root" dans body de l'index.html pour afficher react
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
