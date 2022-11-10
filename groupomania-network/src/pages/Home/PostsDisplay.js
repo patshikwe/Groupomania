f373ea450951fe6dcf81977abe7d8b5089387065
patshikwe <patshikwe@yahoo.fr>
2022-11-10 07:50:00 +0100

    Ajout fichier pour afficher les posts

---
+++ groupomania-network/src/pages/Home/PostsDisplay.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { Card } from '../../utils/style/StyleAdd'
+
+const token = localStorage.getItem('token')
+
+export function PostsDisplay() {
+  const [posts, setPosts] = useState(null)
+  const getAllMessages = async () => {
+    await axios({
+      method: 'get',
+      url: `${process.env.REACT_APP_API_URL}api/post`,
+      headers: {
+        'content-type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        console.log(res)
+        setPosts(res.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  useEffect(() => {
+    getAllMessages()
+  }, [])
+
+  console.log(posts)
+
+  return (
+    <div className="global column-reverse last-child">
+      <h2>Affichage des posts</h2>
+      {posts &&
+        posts.map((post) => (
+          <Card key={post._id}>
+            <p>{post.message}</p>
+          </Card>
+        ))}
+    </div>
+  )
+}
+
+export default PostsDisplay
